54736b16fc0c837a987b9a160e50e9f7e4587493
CodingCas <casweeno2000@gmail.com>
2022-11-08 22:46:49 +0100

    fixes

--- contracts/MetaTokenSender.sol
+++ contracts/MetaTokenSender.sol
@@ -1,19 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract RandomToken is ERC20 {
-    constructor() ERC20("", "") {}
-
-    function freeMint(uint amount) public {
-        _mint(msg.sender, amount);
-    }
-}
-
 contract TokenSender {
-
     using ECDSA for bytes32;
 
     // New mapping
---
+++ contracts/RandomToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract RandomToken is ERC20 {
+    constructor() ERC20("", "") {}
+
+    function freeMint(uint amount) public {
+        _mint(msg.sender, amount);
+    }
+}
